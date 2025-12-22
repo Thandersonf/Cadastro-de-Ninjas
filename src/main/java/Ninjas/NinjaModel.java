672badc48fe378009278738a27fc23c52bb56778
Thanderson <thandersonf@gmail.com>
2025-12-22 20:11:46 -0300

    Adicionado @Collumn na minha classe

--- src/main/java/Ninjas/NinjaModel.java
+++ src/main/java/Ninjas/NinjaModel.java
@@ -11,21 +11,25 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tb_cadastro")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "idade")
     private int idade;
 
     //@manyToOne 1 ninja para uma missao
+
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Chave estrangeira
     private MissoesModel missoes;

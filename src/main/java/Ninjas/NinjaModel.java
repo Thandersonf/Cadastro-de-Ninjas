e03e30ae494ba853cee0ad5c93cfdae271806e5a
Thanderson <thandersonf@gmail.com>
2025-12-22 19:41:41 -0300

    Atualizando o NinjaModel implementando os construtores do Lombok

--- src/main/java/Ninjas/NinjaModel.java
+++ src/main/java/Ninjas/NinjaModel.java
@@ -2,55 +2,32 @@ package Ninjas;
 
 import Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Entity transforma uma classe em uma entidade do BD
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
     //@manyToOne 1 ninja para uma missao
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
